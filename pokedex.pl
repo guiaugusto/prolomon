6efae51f305a788ce99253e2ea34599f0b9554d1
Guilherme Augusto <guilherme.francais@gmail.com>
2018-12-03 18:26:41 -0200

    Creates a method to list pokemons by id

--- pokedex.pl
+++ pokedex.pl
@@ -67,6 +67,23 @@ list_pokemons_type(FirstType) :-
     write('--------------------------'), nl)), nl, nl,
     start_use.
 
+list_pokemons_id(Id) :-
+    nl, findall(Id, pokemon(Id, _, _, _, _, _, _, _, _, _, _, _), Lista),
+    forall(pokemon(Id, NomePokemon, FirstType, SecondType, Total, HP, Attack, Defense, SpAtk, SpDef, Speed, Generation),
+    (nl, write('--------INFORMAÇÃO--------'), nl,
+    write('ID: '),write(Id), nl,
+    write('Nome: '),write(NomePokemon), nl,
+    write('Tipo: '),write(FirstType), write(' - '), write(SecondType), nl,
+    write('HP: '),write(HP), nl,
+    write('Attack: '),write(Attack), nl,
+    write('Defense: '),write(Defense), nl,
+    write('SpAtk: '),write(SpAtk), nl,
+    write('SpDef: '),write(SpDef), nl,
+    write('Speed: '),write(Speed), nl,
+    write('Total: '),write(Total), nl,
+    write('Geração: '),write(Generation), nl,
+    write('--------------------------'), nl)), nl, nl,
+    start_use.
 
 end_pokedex :-
     nl, nl, write('Volte sempre!'), nl, nl, !, halt.
